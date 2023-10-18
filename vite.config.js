9017e795f4e3cd4f2caa3d7e0256c8f11b3e2503
aziz <aziz.chniti@esprit.tn>
2023-10-18 10:44:53 +0100

    publish

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
+  // base: '/Portfolio/',
+  build: {
+    outDir: 'dist', // Set the output directory to 'dist'
+  },
 
 })
